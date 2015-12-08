4a2775f3bb65588d6bdde5d1de41d4b8b9afcc86
Jim Holland <jim@3ex.com>
2015-12-08 11:56:11 +0000

    Initial commit

---
+++ axsyTest/axsyTest/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  axsyTest
+//
+//  Created by James Holland on 08/12/2015.
+//  Copyright © 2015 The Digital Forge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
